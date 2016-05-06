1263f74f2b6cb55e4f6b01113a99337497ea2b0b
hazanpro <as4ny0o@gmail.com>
2016-05-06 23:39:04 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,10 @@ use Flarum\Event\ConfigureClientView;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
+	$events->subscribe(Listener\ChangeDirection::class);
+};
+
+return function (Dispatcher $events) {
 	$events->listen(ConfigureClientView::class, function(ConfigureClientView $event) {
 		if ($event->isForum()) {
 			$event->addAssets([__DIR__ . '/less/forum/rtl.less']);
