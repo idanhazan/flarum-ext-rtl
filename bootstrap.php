756eb20bfa8d0f813c98079ccbc9e22d6a896272
hazanpro <as4ny0o@gmail.com>
2016-05-06 18:14:49 +0300

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+use Flarum\Event\ConfigureClientView;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events) {
+	$events->listen(ConfigureClientView::class, function(ConfigureClientView $event) {
+		if ($event->isForum()) {
+      $event->addAssets([
+        __DIR__ . '/less/forum/rtl.less'
+      ]);
+    }
+	});
+};
