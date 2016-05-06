cf337e23cae2acf4218bdfe31bc201c4eabc5bf6
hazanpro <as4ny0o@gmail.com>
2016-05-07 00:07:20 +0300

    Update ChangeDirection.php

--- src/Listener/ChangeDirection.php
+++ src/Listener/ChangeDirection.php
@@ -1,3 +1,24 @@
 <?php
 
-?>
+namespace Hazan\RTL\Listener;
+
+use Flarum\Event\ConfigureClientView;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class ChangeDirection
+{
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(ConfigureClientView::class, [$this, 'addAssets']);
+    }
+    
+    public function addAssets(ConfigureClientView $event)
+    {
+        if ($event->isForum()) {
+			$event->addAssets([__DIR__ . '/../../less/forum/rtl.less']);
+        }
+        else if($event->isAdmin()) {
+            $event->addAssets([__DIR__ . '/../../less/admin/rtl.less']);
+        }
+    }
+}
