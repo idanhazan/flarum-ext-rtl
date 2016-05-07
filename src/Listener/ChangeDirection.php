4b4e4427205ce4b5f78dccf6b61552cd36797dfe
Idan Hazan <as4ny0o@gmail.com>
2016-05-07 05:13:08 +0300

    Update ChangeDirection.php

--- src/Listener/ChangeDirection.php
+++ src/Listener/ChangeDirection.php
@@ -16,7 +16,7 @@ class ChangeDirection
     {
         if ($event->isForum()) {
 		//$event->addAssets([__DIR__ . '/../../less/forum/rtl.less']);
-		//$event->addAssets([__DIR__ . '/../../less/forum/direction.less']);
+		$event->addAssets([__DIR__ . '/../../less/forum/direction.less']);
         }
         else if($event->isAdmin()) {
 		$event->addAssets([__DIR__ . '/../../less/admin/rtl.less']);
